659be2cff29598c36460278c892bd0a5def8b0e2
dominik.filip10@gmail.com <dominik.filip10@gmail.com>
2018-01-27 12:14:41 +0100

    basic structure and routing

---
+++ src/components/App/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+
+import { Home } from '../../routes/Home/Home'
+import { Login } from '../../routes/Login/Login'
+import './App.css'
+
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path='/' component={ Home }/>
+                    <Route path='/login' component={ Login }/>
+                </Switch>
+            </BrowserRouter>
+        )
+    }
+}
+
+export default App
---
+++ src/routes/Login/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Login extends Component {
+    render() {
+        return (
+            <div>
+                <button className='btn btn-primary'>SIGN IN</button>
+            </div>
+        )
+    }
+}
